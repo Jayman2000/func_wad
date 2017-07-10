12735c3f1dedb641148457e4a6f91a236f3e9ed5
Jason Albert Yundt <swagfortress@gmail.com>
2017-07-10 13:45:00 -0400

    Made wad id length a macro

--- test/test.c
+++ test/test.c
@@ -1,12 +1,14 @@
 #define TEST_WAD "doom2.wad"
+//header
+#define IDENTIFICATION_LENGTH 4
 
 #include <stdio.h>
 
 int main(){
 	FILE *restrict wad = fopen(TEST_WAD, "r");
 	//header                  V descriptions taken from 
-	char identification[5]; //The ASCII characters "IWAD" or "PWAD"
-	fgets(identification, 5, wad);
+	char identification[IDENTIFICATION_LENGTH + 1]; //The ASCII characters "IWAD" or "PWAD"
+	fgets(identification, IDENTIFICATION_LENGTH + 1, wad);
 	
 	puts("Header:");
 	printf(" identification == %s\n", identification);

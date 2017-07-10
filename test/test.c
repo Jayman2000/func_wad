2e23579bb631c8953cf727e7d4e7eb7cd68df573
Jason Albert Yundt <swagfortress@gmail.com>
2017-07-10 13:37:58 -0400

    Prints identification

--- test/test.c
+++ test/test.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+
 int main(){
+	FILE *restrict wad = fopen("doom2.wad", "r");
+	//header                  V descriptions taken from 
+	char identification[5]; //The ASCII characters "IWAD" or "PWAD"
+	fgets(identification, 5, wad);
+	
+	puts("Header:");
+	printf(" identification == %s\n", identification);
 	return 0;
-} 
+}
